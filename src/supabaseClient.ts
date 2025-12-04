e40b3bf1e558bd1b61fafdf3bd25029d69f5eb09
d9linkx <prince.chidera.dike@gmail.com>
2025-12-04 17:32:02 +0100

    API key error

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   'https://iujxrjqasrfcbdmrrirn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlveXdxYWFmd2p6cm9pcGxia2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4Mzk2MjMsImV4cCI6MjA3OTQxNTYyM30.cwD-5j8qSNRcPH-iEbfh8ge9PA-GUlc90PIDQ3Yb1FQ'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1anhyanFhc3JmY2JkbXJyaXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NDMzMzQsImV4cCI6MjA4MDQxOTMzNH0.FMSG1PJRxQD0ZVXFAMsgK6hehAvLLIJBmMaC6dkhxLo'
 );
