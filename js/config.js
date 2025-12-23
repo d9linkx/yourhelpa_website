0bb8aec01906f6b8de8cd74388bd54ff6c0b480a
Prince <prince.chidera.dike@gmail.com>
2025-12-24 00:27:08 +0100

    Un-ignore config.js and fix casing for Vercel

---
+++ js/config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = 'https://iujxrjqasrfcbdmrrirn.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1anhyanFhc3JmY2JkbXJyaXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NDMzMzQsImV4cCI6MjA4MDQxOTMzNH0.FMSG1PJRxQD0ZVXFAMsgK6hehAvLLIJBmMaC6dkhxLo';
