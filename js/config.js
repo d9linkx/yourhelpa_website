f30f4db147fe7a056e4da973862da9967b62f6ed
Prince <prince.chidera.dike@gmail.com>
2025-12-25 14:07:36 +0100

    new

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-// Since we are not on Vercel anymore, we must define these manually.
-// Replace the strings below with your actual Supabase Project URL and Anon Key.
+// Supabase Configuration
 window.SUPABASE_URL = "https://iujxrjqasrfcbdmrrirn.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1anhyanFhc3JmY2JkbXJyaXJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NDMzMzQsImV4cCI6MjA4MDQxOTMzNH0.FMSG1PJRxQD0ZVXFAMsgK6hehAvLLIJBmMaC6dkhxLo";
